9f5b585bd66d3c37b95813bb3d9fa04bdc740ba2
Adrian Garic <adrian.garic007@gmail.com>
2025-09-13 23:50:09 +0200

    izbrisane stavke i slike

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,3 @@
-// filepath: c:\Users\adrij\OneDrive\Documents\GITHUB\WA-projekt\frontend\babel.config.js
 module.exports = {
-    presets: ['@vue/cli-plugin-babel/preset']
-  };
+  presets: ["@vue/cli-plugin-babel/preset"],
+};
